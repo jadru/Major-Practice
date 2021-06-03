2672f82b71453b44f8d565d1b69532209e2ae8bb
Younggun Park <ygpark0922@gmail.com>
2021-06-03 16:43:08 +0900

    Create prac_3.cpp

---
+++ C++/for Final_Exam/prac_3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class MyString{
+  MyString operator - (Mystring op2){
+    
+  }
+  MyString operator + (Mystring op2){
+    
+  }
+};
+
+int main(void){
+  return 0;
+}
